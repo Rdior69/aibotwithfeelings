acaadef6c076042a7e79e7959fcfdb0a6934b6eb
Matthew Ward <raydiore@gmail.com>
2026-05-29 02:40:40 -0700

    Initial Commit

---
+++ aibotwithfeelings/aibotwithfeelingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  aibotwithfeelingsApp.swift
+//  aibotwithfeelings
+//
+//  Created by ray dior on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct aibotwithfeelingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
